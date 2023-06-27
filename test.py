be08c2ce8700a005c78fe1602197ae7f3e3b589e
dustin <dnv22@engr.msstate.edu>
2023-06-27 14:12:18 -0500

    rm

--- test.py
+++
@@ -1,9 +0,0 @@
-date = input('Enter julian date YYYYDDD YYYYDDDD: ')
-
-date = date.split(' ')
-
-print('year:', date[0][0:4])
-print('day', date[0][4:7])
-
-print('year:', date[1][0:4])
-print('day', date[1][4:7])
