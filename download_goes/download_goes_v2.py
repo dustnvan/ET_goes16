533bd4327456d58efdbb79cf54f7f44ac438ed08
dustin <dnv22@engr.msstate.edu>
2023-06-27 13:32:28 -0500

    improved down and geotiff export time selection

--- download_goes/download_goes_v2.py
+++ download_goes/download_goes_v2.py
@@ -1,19 +1,19 @@
-import os.path
 import requests
-import h5py
 import time
-
+import os
 
 # checks if website is running
-def website_check():
+def make_request(url, stream=False, timeout=None):
+    # uncomment if website check not needed
+    # return True
+
     retry_delay = 1
 
-    url = 'https://data.nas.nasa.gov/geonex/data.php'
     while True:
         try:
-            response = requests.head(url)
+            response = requests.get(url_base, stream=stream, timeout=timeout)
             if response.status_code == 200:
-                return True
+                return response
             else:
                 print(f"Website is not available. Pausing script... {retry_delay} seconds")
                 time.sleep(retry_delay)  # Pause for 60 seconds before checking again
@@ -24,114 +24,70 @@ def website_check():
 
         retry_delay *= 2
 
+
 # finds the URLS of the hdf file downloads
 def get_hdf(url_base):
-    if website_check():
-        hdf_list = []
-        url = requests.get(url_base)
-        htmltext = url.text
+    hdf_list = []
+    url = make_request(url_base)
+    htmltext = url.text
 
-        for line in htmltext.split('\n'):
-            if 'href="/geonex' in line:
-                line_hdf = line.split('href="')[1]
-                if 'hdf' in line_hdf and 'ABI12B' in line_hdf:
-                    line_hdf1 = "https://data.nas.nasa.gov" + line_hdf.split(".hdf")[0] + ".hdf"
-                    hdf_list.append(line_hdf1)
-        return hdf_list
+    for line in htmltext.split('\n'):
+        if 'href="/geonex' in line:
+            line_hdf = line.split('href="')[1]
+            if 'hdf' in line_hdf and 'ABI12B' in line_hdf:
+                line_hdf1 = "https://data.nas.nasa.gov" + line_hdf.split(".hdf")[0] + ".hdf"
+                hdf_list.append(line_hdf1)
+    return hdf_list
 
 
 # downloads hdf file
-def download_file(url, filename):
-    if website_check():
-        # NOTE the stream=True parameter below
-        with requests.get(url, stream=True, timeout=600) as r:
-            r.raise_for_status()
-            with open(filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    # If you have chunk encoded response uncomment if
-                    # and set chunk_size parameter to None.
-                    # if chunk:
-                    f.write(chunk)
-
-
-# checks for not downloaded or corrupt files
-def check_hdf_file(file_path):
-    try:
-        # Open the HDF file in read-only mode
-        with h5py.File(file_path, 'r') as file:
-            # Access any dataset or group to ensure the file is readable
-            _ = file['/']
-            print("HDF file is valid and can be opened.")
-            return 1
-    except OSError as e:
-        print("Error opening HDF file:", e)
-        return 0
-    except KeyError as e:
-        print("HDF file is corrupt:", e)
-        return 0
-
-
-# does a file check and attempts to redownload if bad file
-def redownload_hdf_files(url, filename):
-    attempts = 0
-    max_attempts = 3
-
-    while attempts < max_attempts:
-        if not check_hdf_file(filename):
-            print("File not opening. Redownloading...")
-            download_file(url, filename)
-        else:
-            return
-        attempts += 1
-
-    print("File cannot be downloaded. Skipping file.")
+def download_file(url, hdf):
+    # NOTE the stream=True parameter below
+    with make_request(url, stream=True, timeout=600) as r:
+        r.raise_for_status()
+        with open(hdf, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                # if chunk:
+                f.write(chunk)
+            print('downloaded:', hdf)
 
 
 if __name__ == '__main__':
-    # basiclink = 'https://data.nas.nasa.gov/geonex/geonexdata/GOES16/GEONEX-L1G/'
-    # output_dir = 'C:\datasets\images\goes\goes16\geonexl1g'
-
     basiclink = 'https://data.nas.nasa.gov/geonex/geonexdata/GOES16/GEONEX-L2/MAIAC'
-    # output_dir = 'Y:\datasets\images\goes\goes16\geonexl2\maiac'
-    output_dir = 'datasets\images\goes\goes16\geonexl2\maiac'
+    output_dir = r'Z:\dustin\goes\geonex_l2\data_raw'
 
-    years_init = 2018
-    years_end = 2019
+    date = input('Enter julian date start and end YYYYDDD YYYYDDD: ')
+    date = date.split(' ')
 
-    dates_init = 1
-    dates_end = 365
-    tiles = ['h14v04']  # Amazon 'h20v10', 'h21v10', 'h19v10','h20v09'  # MS h15v04 h14v04 h14v05 h15v05
+    start_date = int(date[0])
+    end_date = int(date[1])
 
-    hdf_file_map = {}  # file name : download url
+    tile = input('Choose a tile (h14v03 h14v04 h15v03 h15v04): ')
 
-    for tile in tiles:
-        for year in range(years_init, years_end+1):
-            for day in range(dates_init, dates_end + 1):
-                # Creating directories
-                year = str(year).zfill(4)
-                day = str(day).zfill(3)
+    current_date = start_date
+    while current_date <= end_date:
+        current_yr = str(current_date)[0:4]
+        current_julian = str(current_date)[4:7]
 
-                dir_yrdt = f'{output_dir}\\{tile}\\{year}\\{day}\\'
-                os.makedirs(dir_yrdt, exist_ok=True)
+        dir_dst = os.path.join(output_dir, tile, current_yr, current_julian)
+        os.makedirs(dir_dst, exist_ok=True)
 
-                # getting hdf file urls
-                url_base = f'{basiclink}/{tile}/{year}/{day}/'
-                hdf_list = get_hdf(url_base)
+        # getting hdf file urls
+        url_base = f'{basiclink}/{tile}/{current_yr}/{current_julian}/'
+        hdf_list = get_hdf(url_base)  # contains all the links for hdf downloads in the day
 
-                # creating hdf file name and linking it to its URL
-                for hdf_file in hdf_list:
-                    filename_save = rf'{dir_yrdt}\{os.path.basename(hdf_file)}'
-                    if not os.path.exists(filename_save):
-                        hdf_file_map[filename_save] = hdf_file
-                        # filename_saves.append(filename_save)
-                        # hdf_links.append(hdf_file)
+        # creating hdf file path and linking it to its URL
+        for hdf_file in hdf_list:
+            hdf_path = os.path.join(dir_dst, os.path.basename(hdf_file))
+            if not os.path.exists(hdf_path):
+                # downloads file
+                download_file(hdf_file, hdf_path)
+            else:
+                print(hdf_path, 'already exists')
 
-                # downloads hdf file to its corresponding directory
-                for filename, hdf_link in hdf_file_map.items():
-                    download_file(hdf_link, filename)
-                    print(f"downloaded: {hdf_link}")
+        current_date += 1
 
-    # checks for files not downloaded or corrupted
-    # for filename, hdf_link in hdf_file_map.items():
-    #     redownload_hdf_files(filename, hdf_link)
+
 
--- functions.py
+++ functions.py
@@ -46,12 +46,7 @@ print(local_time)
 
 
 
-
-
-
-
-
-
+from osgeo import gdal
 
 # Finding Pixel
 def find_pixel(geotiff_path):
@@ -101,73 +96,3 @@ def get_reflectance_value(geotiff_path, y, x):
     return band_array[y][x]
 
 
-
-import numpy as np
-# calculating SAVI and NDVI
-
-# calculate savi from landsat bands and export geotiff
-# make mask
-def calculate_ndvi(nir, red):
-    valid_pixels = (nir >= 0) & (red >= 0)
-    ndvi_band = np.zeros_like(nir, dtype=np.float32)
-    ndvi_band = np.where(valid_pixels, (nir - red) / (nir + red), np.nan)
-    return ndvi_band
-
-
-
-from osgeo import gdal
-
-# Function to calculate SAVI
-def calculate_savi(nir, red):
-    soil_factor = 0.5
-    valid_pixels = (nir >= 0) & (red >= 0)
-    savi_band = np.where(valid_pixels,((1 + soil_factor) * (nir - red)) / (nir + red + soil_factor),np.nan)
-    return savi_band
-
-def export_geotiff(src_dataset, band, output_path):
-    # Get the geotransform from the NIR dataset
-    geotransform = src_dataset.GetGeoTransform()
-
-    # Create the output GeoTIFF
-    driver = gdal.GetDriverByName('GTiff')
-    output_dataset = driver.Create(output_path, src_dataset.RasterXSize, src_dataset.RasterYSize, 1, gdal.GDT_Float32)
-
-    # Set the geotransform and projection
-    output_dataset.SetGeoTransform(geotransform)
-    output_dataset.SetProjection(src_dataset.GetProjection())
-
-    # Write the SAVI band to the output GeoTIFF
-    output_band = output_dataset.GetRasterBand(1)
-    output_band.WriteArray(band)
-
-    # Flush data to disk and close the output GeoTIFF
-    output_band.FlushCache()
-    output_dataset.FlushCache()
-    output_dataset = None
-
-def export_savi_ndvi(nir_path, red_path):
-    savi_output_path = nir_path.replace("nir", "savi")
-    ndvi_output_path = nir_path.replace("nir", "ndvi")
-
-    # Open NIR and red GeoTIFF files
-    nir_dataset = gdal.Open(nir_path)
-    red_dataset = gdal.Open(red_path)
-
-    # Read NIR and red bands as NumPy arrays
-    nir_band = nir_dataset.GetRasterBand(1).ReadAsArray()
-    red_band = red_dataset.GetRasterBand(1).ReadAsArray()
-
-    savi_band = calculate_savi(nir_band, red_band)
-    ndvi_band = calculate_ndvi(nir_band, red_band)
-
-    export_geotiff(nir_dataset, savi_band, savi_output_path)
-    export_geotiff(nir_dataset, ndvi_band, ndvi_output_path)
-
-
-# Paths to NIR and red GeoTIFF files
-# nir_path = r'C:\Users\dusti\Desktop\GCERlab\ET_goes16\download_goes\datasets\images\goes\goes16\geonexl2\geotiffs\h14v04\2018\001\1600\nir_GO16_ABI12B_20180011600_GLBG_h14v04_02_proj.tif'
-# red_path = r'C:\Users\dusti\Desktop\GCERlab\ET_goes16\download_goes\datasets\images\goes\goes16\geonexl2\geotiffs\h14v04\2018\001\1600\red_GO16_ABI12B_20180011600_GLBG_h14v04_02_proj.tif'
-
-nir_path = r'C:\Users\dnv22\Desktop\ET_goes16\download_goes\datasets\images\goes\goes16\geonexl2\geotiffs\h14v04\2018\001\1600\nir_GO16_ABI12B_20180011600_GLBG_h14v04_02_proj.tif'
-red_path= r'C:\Users\dnv22\Desktop\ET_goes16\download_goes\datasets\images\goes\goes16\geonexl2\geotiffs\h14v04\2018\001\1600\red_GO16_ABI12B_20180011600_GLBG_h14v04_02_proj.tif'
-export_savi_ndvi(nir_path, red_path)
