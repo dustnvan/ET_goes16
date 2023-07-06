632e4f3549f9bf2fac23cd3ab743a6cb41146e09
dustin <dnv22@engr.msstate.edu>
2023-07-06 13:25:34 -0500

    fixed mask

--- goes_export_geotiff/export_savi_ndvi.py
+++ goes_export_geotiff/export_savi_ndvi.py
@@ -4,15 +4,15 @@ import numpy as np
 
 noDataVal = -28672
 def calculate_ndvi(nir, red):
-    valid_mask = np.where(nir!=noDataVal, True, False) & np.where(red!=noDataVal, True,False)
+    valid_mask = (nir != noDataVal) & (red != noDataVal)
     ndvi_band = np.where(valid_mask, (nir - red) / (nir + red), np.nan)
     return ndvi_band
 
 # Function to calculate SAVI
 def calculate_savi(nir, red):
     soil_factor = 0.5
-    valid_pixels = (nir > noDataVal) & (red > noDataVal)
-    savi_band = np.where(valid_pixels,((1 + soil_factor) * (nir - red)) / (nir + red + soil_factor),np.nan)
+    valid_mask = (nir != noDataVal) & (red != noDataVal)
+    savi_band = np.where(valid_mask,((1 + soil_factor) * (nir - red)) / (nir + red + soil_factor),np.nan)
     return savi_band
 
 def export_geotiff(src_dataset, band, output_path):
